9ee6b045ebcd7e643b6541a45147de379386abcd
lananh243 <147390706+lananh243@users.noreply.github.com>
2023-11-10 09:20:13 +0700

    Add files via upload

---
+++ bt1 ss4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int number;
+	printf("Nhap vao mot so nguyen:");
+	scanf("%d",&number);
+	if(number %3 ==0 && number % 5 == 0) {
+		printf("%d chia het cho ca 3 va 5\n", number);
+	} else {
+		printf("%d khong chia het cho ca 3 va 5\n",number);	
+	}
+	}
